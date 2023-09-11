0f251fa86578b5ac168008ae367708111a93de33
AbdullaMammadov <abdullaam@code.edu.az>
2023-09-11 17:22:48 +0400

    first commit

---
+++ Model/Marka.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkaModel.Models
+{
+    public class Marka
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int AnnualIncome { get; set; }
+
+    }
+}
---
+++ Model/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkaModel.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MarkaId { get; set; }
+        public Marka Marka { get; set; }
+
+    }
+}
